c9189d25ca934a853dd753b0a85b1bfc6d7bfc55
kardelendemiral <74200767+kardelendemiral@users.noreply.github.com>
2021-04-11 17:08:10 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ int main(int argc, char* argv[]) {
     outfile.open(outfileName);
 
     vector<string> vars; //variable'lar bu vectorde hep
+    bool syntaxError=false;
+    int tmpCount=0;
 
     outfile << "; ModuleID = 'mylang2ir'\n"
                "declare i32 @printf(i8*, ...)\n"
@@ -64,39 +66,47 @@ int main(int argc, char* argv[]) {
     while(getline(infile,line)){
         int found=line.find("=");
         bool a=true;
-        if(found!=string::npos){
-            string s=line.substr(found+1);
-            if(s.find("+")==-1 &&s.find("-")==-1&&s.find("*")==-1&&s.find("/")==-1){ //toplama vs yoksa
-                for(int i=0; i<s.length();i++){
-                    if(s.at(i)==32){ //whitespace ise geçiyo
-                        continue;
-                    }
-                    if(s.at(i)<48 || s.at(i)>57){ // [0-9] değilse çıkıyo (ascii tablo şeyi)
+        if(found!=string::npos){ //ASSIGNMENT STATEMENT
+            string sag=line.substr(found+1); //assignmentin sag kısmı
+            string sol=line.substr(0,found); //assignmentın sol kısmı
+
+            while(sag[0]==' '){  //bastaki boslukları sil
+                sag=sag.substr(1);
+            }
+            while(sag[sag.size()-1]==' '){  //sondaki boslukları sil
+                sag=sag.substr(0,sag.size()-1);
+            }
+            while(sol[0]==' '){  //bastaki boslukları sil
+                sol=sol.substr(1);
+            }
+            while(sol[sol.size()-1]==' '){  //sondaki boslukları sil
+                sol=sol.substr(0,sol.size()-1);
+            }
+
+            if(sol.find(" ")!=-1){
+                syntaxError=true;
+                cout << "ERROR";
+            }
+
+            if(sag.find("+")==-1 &&sag.find("-")==-1&&sag.find("*")==-1&&sag.find("/")==-1){ //toplama vs yoksa
+                if(sag.find(" ")!=-1){ //islem yoksa sag tarafta sadece bi sey vardir. sondaki ve bastaki boslukları sildiğimiz için bosluk varsa error
+                    syntaxError=true;
+                    cout << "ERROR";
+                }
+
+                for(int i=0; i<sag.length();i++){
+                    if(sag.at(i)<48 || sag.at(i)>57 ){ // [0-9] değilse çıkıyo (ascii tablo şeyi)
                         a=false;
                         break;
                     }
                 }
                 if(a==true){ //[0-9] ve toplama vs yok
-                    //bu kısım biraz inefficient oldu daha mantıklı bişey bulabiliriz
-                    string bef=line.substr(0,found); //variable nameini alıyom bu satırlarda+
-                    int n =bef.length();
-                    char a[n+1];
-                    strcpy(a,bef.c_str());
-                    char* name = strtok(a," ");//+
-                    int k =s.length();
-                    char a2[n+1];
-                    strcpy(a2,s.c_str());
-                    char* value = strtok(a2," ");//bu da valuesu
-                    outfile<<"store i32 "<<value<<", i32* %"<<name<<endl;
-                    //NOTLARRR:
-                    /* burda toplama vs yoksa = den sonra sadece 1 sayının olduğunu assume ettim eğer syntax
-                     * hatası falan verirlerse sıçmak mesela k=   verirse olmuyo aynı şekilde a= 3   2  da olmaz
-                     * ama  boşlukları falan hallediyo
-                     * strtok yerine bişey bulsak iyi olcak :(
-                    */
-
+                    outfile<<"store i32 "<<sag<<", i32* %"<<sol<<endl;
                 }
             }
+            //ASIL KABUS BURADA BASLIYOR :(((
+
+
 
         }
 
